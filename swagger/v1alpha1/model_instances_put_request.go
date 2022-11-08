f65ded69c5e926f99dc9d98f117efe31f32067da
Gitlab Runner <tkralj@crusoeenergy.com>
2022-11-08 22:59:10 +0000

    Client code changes for (Committed use) in rest-gw

--- swagger/v1alpha1/model_instances_put_request.go
+++ swagger/v1alpha1/model_instances_put_request.go
@@ -10,5 +10,6 @@
 package swagger
 
 type InstancesPutRequest struct {
-	Action string `json:"action"`
+	Action           string `json:"action"`
+	CommitmentPeriod int64  `json:"commitment_period,omitempty"`
 }
--- swagger/v1alpha2/model_instance.go
+++ swagger/v1alpha2/model_instance.go
@@ -10,11 +10,13 @@
 package swagger
 
 type Instance struct {
-	CreatedAt      string `json:"created_at,omitempty"`
-	Id             string `json:"id,omitempty"`
-	Name           string `json:"name,omitempty"`
-	ProductName    string `json:"product_name,omitempty"`
-	RoleId         string `json:"role_id,omitempty"`
-	SshDestination string `json:"ssh_destination,omitempty"`
-	State          string `json:"state,omitempty"`
+	CommitmentEnd    string `json:"commitment_end,omitempty"`
+	CommitmentPeriod int64  `json:"commitment_period,omitempty"`
+	CreatedAt        string `json:"created_at,omitempty"`
+	Id               string `json:"id,omitempty"`
+	Name             string `json:"name,omitempty"`
+	ProductName      string `json:"product_name,omitempty"`
+	RoleId           string `json:"role_id,omitempty"`
+	SshDestination   string `json:"ssh_destination,omitempty"`
+	State            string `json:"state,omitempty"`
 }
