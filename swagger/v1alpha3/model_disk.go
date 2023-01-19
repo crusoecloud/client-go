db52b0b48d27e92b8d727d8bc6ef638877dd77c7
Gitlab Runner <syang@crusoeenergy.com>
2023-01-19 23:50:58 +0000

    Client code changes for (Add created timestamp to disks) in rest-gw

--- swagger/v1alpha3/model_disk.go
+++ swagger/v1alpha3/model_disk.go
@@ -11,6 +11,7 @@ package swagger
 
 type Disk struct {
 	AttachedTo []VmAttachment `json:"attached_to,omitempty"`
+	CreatedAt  string         `json:"created_at,omitempty"`
 	Id         string         `json:"id,omitempty"`
 	Location   string         `json:"location,omitempty"`
 	Name       string         `json:"name,omitempty"`
