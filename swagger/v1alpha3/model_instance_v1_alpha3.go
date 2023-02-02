c5f3f212d157493598c27c6cff65fe5abe5093e9
Gitlab Runner <jcameron@crusoeenergy.com>
2023-02-02 22:37:56 +0000

    Client code changes for (Change the name in VM attached disks to just disks) in rest-gw

--- swagger/v1alpha3/model_instance_v1_alpha3.go
+++ swagger/v1alpha3/model_instance_v1_alpha3.go
@@ -11,10 +11,10 @@ package swagger
 
 // Instance contains identifying information about a vm instance for vms.instances endpoints.
 type InstanceV1Alpha3 struct {
-	AttachedDisks    []AttachedDisk `json:"attached_disks,omitempty"`
 	CommitmentEnd    string         `json:"commitment_end,omitempty"`
 	CommitmentPeriod int64          `json:"commitment_period,omitempty"`
 	CreatedAt        string         `json:"created_at,omitempty"`
+	Disks            []AttachedDisk `json:"disks,omitempty"`
 	Id               string         `json:"id,omitempty"`
 	Name             string         `json:"name,omitempty"`
 	ProductName      string         `json:"product_name,omitempty"`
