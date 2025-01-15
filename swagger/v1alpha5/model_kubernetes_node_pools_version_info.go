f68c4dd8fb4dd72e8bc46018aecaa1a3ec8f0814
Gitlab Runner <nchaler@crusoeenergy.com>
2025-01-15 01:16:04 +0000

    Client code changes for (Merge branch 'k8s-versions-details' into 'master') in rest-gw

--- swagger/v1alpha5/model_kubernetes_node_pools_version_info.go
+++ swagger/v1alpha5/model_kubernetes_node_pools_version_info.go
@@ -12,5 +12,6 @@ type KubernetesNodePoolsVersionInfo struct {
 	// The version name of Crusoe's Kubernetes node pools.
 	NodePoolVersionName string `json:"node_pool_version_name"`
 	// A list of tags associated with the version.
-	Tags []string `json:"tags"`
+	Tags           []string        `json:"tags"`
+	VersionDetails *VersionDetails `json:"version_details"`
 }
