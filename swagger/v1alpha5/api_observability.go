91b083a51b8c152099b65f2f7568b3b0f338a99e
Gitlab Runner <asalomon@crusoeenergy.com>
2025-03-18 17:31:26 +0000

    Client code changes for (Merge branch 'add-metrics-proxy-cleanup-api' into 'master') in rest-gw

--- swagger/v1alpha5/api_observability.go
+++ swagger/v1alpha5/api_observability.go
@@ -27,21 +27,24 @@ var (
 type ObservabilityApiService service
 
 /*
-ObservabilityApiService Query timeseries for a resource.
+ObservabilityApiService Query timeseries at an instant via PromQL.
  * @param ctx context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
  * @param projectId
+ * @param query Prometheus expression query string.
  * @param optional nil or *ObservabilityApiQueryTimeseriesOpts - Optional Parameters:
-     * @param "ResourceType" (optional.String) -
-     * @param "ResourceId" (optional.String) -
+     * @param "Time" (optional.String) -  Evaluation timestamp.
+     * @param "Timeout" (optional.String) -  Evaluation timeout. Defaults to and is capped by the value of the -query.timeout flag.
+     * @param "Limit" (optional.String) -  Maximum number of returned series. 0 means disabled.
 @return ObservabilityMetrics
 */
 
 type ObservabilityApiQueryTimeseriesOpts struct {
-	ResourceType optional.String
-	ResourceId   optional.String
+	Time    optional.String
+	Timeout optional.String
+	Limit   optional.String
 }
 
-func (a *ObservabilityApiService) QueryTimeseries(ctx context.Context, projectId string, localVarOptionals *ObservabilityApiQueryTimeseriesOpts) (ObservabilityMetrics, *http.Response, error) {
+func (a *ObservabilityApiService) QueryTimeseries(ctx context.Context, projectId string, query string, localVarOptionals *ObservabilityApiQueryTimeseriesOpts) (ObservabilityMetrics, *http.Response, error) {
 	var (
 		localVarHttpMethod  = strings.ToUpper("Get")
 		localVarPostBody    interface{}
@@ -58,11 +61,15 @@ func (a *ObservabilityApiService) QueryTimeseries(ctx context.Context, projectId
 	localVarQueryParams := url.Values{}
 	localVarFormParams := url.Values{}
 
-	if localVarOptionals != nil && localVarOptionals.ResourceType.IsSet() {
-		localVarQueryParams.Add("resource_type", parameterToString(localVarOptionals.ResourceType.Value(), ""))
+	localVarQueryParams.Add("query", parameterToString(query, ""))
+	if localVarOptionals != nil && localVarOptionals.Time.IsSet() {
+		localVarQueryParams.Add("time", parameterToString(localVarOptionals.Time.Value(), ""))
 	}
-	if localVarOptionals != nil && localVarOptionals.ResourceId.IsSet() {
-		localVarQueryParams.Add("resource_id", parameterToString(localVarOptionals.ResourceId.Value(), ""))
+	if localVarOptionals != nil && localVarOptionals.Timeout.IsSet() {
+		localVarQueryParams.Add("timeout", parameterToString(localVarOptionals.Timeout.Value(), ""))
+	}
+	if localVarOptionals != nil && localVarOptionals.Limit.IsSet() {
+		localVarQueryParams.Add("limit", parameterToString(localVarOptionals.Limit.Value(), ""))
 	}
 	// to determine the Content-Type header
 	localVarHttpContentTypes := []string{}
@@ -167,21 +174,171 @@ func (a *ObservabilityApiService) QueryTimeseries(ctx context.Context, projectId
 }
 
 /*
-ObservabilityApiService Query timeseries for a resource.
+ObservabilityApiService Query timeseries over a time range via PromQL.
  * @param ctx context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
  * @param projectId
+ * @param query Prometheus expression query string.
+ * @param start Start timestamp, inclusive.
+ * @param end End timestamp, inclusive.
+ * @param step Query resolution step width in duration format or float number of seconds.
  * @param optional nil or *ObservabilityApiQueryTimeseriesRangeOpts - Optional Parameters:
-     * @param "ResourceType" (optional.String) -
-     * @param "ResourceId" (optional.String) -
+     * @param "Timeout" (optional.String) -  Evaluation timeout. Defaults to and is capped by the value of the -query.timeout flag.
+     * @param "Limit" (optional.String) -  Maximum number of returned series. 0 means disabled.
 @return ObservabilityMetrics
 */
 
 type ObservabilityApiQueryTimeseriesRangeOpts struct {
-	ResourceType optional.String
-	ResourceId   optional.String
+	Timeout optional.String
+	Limit   optional.String
 }
 
-func (a *ObservabilityApiService) QueryTimeseriesRange(ctx context.Context, projectId string, localVarOptionals *ObservabilityApiQueryTimeseriesRangeOpts) (ObservabilityMetrics, *http.Response, error) {
+func (a *ObservabilityApiService) QueryTimeseriesRange(ctx context.Context, projectId string, query string, start string, end string, step string, localVarOptionals *ObservabilityApiQueryTimeseriesRangeOpts) (ObservabilityMetrics, *http.Response, error) {
+	var (
+		localVarHttpMethod  = strings.ToUpper("Get")
+		localVarPostBody    interface{}
+		localVarFileName    string
+		localVarFileBytes   []byte
+		localVarReturnValue ObservabilityMetrics
+	)
+
+	// create path and map variables
+	localVarPath := a.client.cfg.BasePath + "/projects/{project_id}/metrics/timeseries/api/v1/query-range"
+	localVarPath = strings.Replace(localVarPath, "{"+"project_id"+"}", fmt.Sprintf("%v", projectId), -1)
+
+	localVarHeaderParams := make(map[string]string)
+	localVarQueryParams := url.Values{}
+	localVarFormParams := url.Values{}
+
+	localVarQueryParams.Add("query", parameterToString(query, ""))
+	localVarQueryParams.Add("start", parameterToString(start, ""))
+	localVarQueryParams.Add("end", parameterToString(end, ""))
+	localVarQueryParams.Add("step", parameterToString(step, ""))
+	if localVarOptionals != nil && localVarOptionals.Timeout.IsSet() {
+		localVarQueryParams.Add("timeout", parameterToString(localVarOptionals.Timeout.Value(), ""))
+	}
+	if localVarOptionals != nil && localVarOptionals.Limit.IsSet() {
+		localVarQueryParams.Add("limit", parameterToString(localVarOptionals.Limit.Value(), ""))
+	}
+	// to determine the Content-Type header
+	localVarHttpContentTypes := []string{}
+
+	// set Content-Type header
+	localVarHttpContentType := selectHeaderContentType(localVarHttpContentTypes)
+	if localVarHttpContentType != "" {
+		localVarHeaderParams["Content-Type"] = localVarHttpContentType
+	}
+
+	// to determine the Accept header
+	localVarHttpHeaderAccepts := []string{"application/json"}
+
+	// set Accept header
+	localVarHttpHeaderAccept := selectHeaderAccept(localVarHttpHeaderAccepts)
+	if localVarHttpHeaderAccept != "" {
+		localVarHeaderParams["Accept"] = localVarHttpHeaderAccept
+	}
+	r, err := a.client.prepareRequest(ctx, localVarPath, localVarHttpMethod, localVarPostBody, localVarHeaderParams, localVarQueryParams, localVarFormParams, localVarFileName, localVarFileBytes)
+	if err != nil {
+		return localVarReturnValue, nil, err
+	}
+
+	localVarHttpResponse, err := a.client.callAPI(r)
+	if err != nil || localVarHttpResponse == nil {
+		return localVarReturnValue, localVarHttpResponse, err
+	}
+
+	localVarBody, err := ioutil.ReadAll(localVarHttpResponse.Body)
+	localVarHttpResponse.Body.Close()
+	if err != nil {
+		return localVarReturnValue, localVarHttpResponse, err
+	}
+
+	if localVarHttpResponse.StatusCode < 300 {
+		// If we succeed, return the data, otherwise pass on to decode error.
+		err = a.client.decode(&localVarReturnValue, localVarBody, localVarHttpResponse.Header.Get("Content-Type"))
+		if err == nil {
+			return localVarReturnValue, localVarHttpResponse, err
+		}
+	}
+
+	if localVarHttpResponse.StatusCode >= 300 {
+		newErr := GenericSwaggerError{
+			body:  localVarBody,
+			error: localVarHttpResponse.Status,
+		}
+		if localVarHttpResponse.StatusCode == 200 {
+			var v ObservabilityMetrics
+			err = a.client.decode(&v, localVarBody, localVarHttpResponse.Header.Get("Content-Type"))
+			if err != nil {
+				newErr.error = err.Error()
+				return localVarReturnValue, localVarHttpResponse, newErr
+			}
+			newErr.model = v
+			return localVarReturnValue, localVarHttpResponse, newErr
+		}
+		if localVarHttpResponse.StatusCode == 400 {
+			var v InlineResponse400
+			err = a.client.decode(&v, localVarBody, localVarHttpResponse.Header.Get("Content-Type"))
+			if err != nil {
+				newErr.error = err.Error()
+				return localVarReturnValue, localVarHttpResponse, newErr
+			}
+			newErr.model = v
+			return localVarReturnValue, localVarHttpResponse, newErr
+		}
+		if localVarHttpResponse.StatusCode == 401 {
+			var v InlineResponse401
+			err = a.client.decode(&v, localVarBody, localVarHttpResponse.Header.Get("Content-Type"))
+			if err != nil {
+				newErr.error = err.Error()
+				return localVarReturnValue, localVarHttpResponse, newErr
+			}
+			newErr.model = v
+			return localVarReturnValue, localVarHttpResponse, newErr
+		}
+		if localVarHttpResponse.StatusCode == 403 {
+			var v InlineResponse403
+			err = a.client.decode(&v, localVarBody, localVarHttpResponse.Header.Get("Content-Type"))
+			if err != nil {
+				newErr.error = err.Error()
+				return localVarReturnValue, localVarHttpResponse, newErr
+			}
+			newErr.model = v
+			return localVarReturnValue, localVarHttpResponse, newErr
+		}
+		if localVarHttpResponse.StatusCode == 500 {
+			var v InlineResponse500
+			err = a.client.decode(&v, localVarBody, localVarHttpResponse.Header.Get("Content-Type"))
+			if err != nil {
+				newErr.error = err.Error()
+				return localVarReturnValue, localVarHttpResponse, newErr
+			}
+			newErr.model = v
+			return localVarReturnValue, localVarHttpResponse, newErr
+		}
+		return localVarReturnValue, localVarHttpResponse, newErr
+	}
+
+	return localVarReturnValue, localVarHttpResponse, nil
+}
+
+/*
+ObservabilityApiService Query timeseries for a resource (in development).
+ * @param ctx context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
+ * @param projectId
+ * @param resourceType
+ * @param start Start timestamp, inclusive.
+ * @param end End timestamp, inclusive.
+ * @param step Query resolution step width in duration format or float number of seconds.
+ * @param optional nil or *ObservabilityApiQueryTimeseriesSimplifiedOpts - Optional Parameters:
+     * @param "ResourceId" (optional.String) -
+@return ObservabilityMetrics
+*/
+
+type ObservabilityApiQueryTimeseriesSimplifiedOpts struct {
+	ResourceId optional.String
+}
+
+func (a *ObservabilityApiService) QueryTimeseriesSimplified(ctx context.Context, projectId string, resourceType string, start string, end string, step string, localVarOptionals *ObservabilityApiQueryTimeseriesSimplifiedOpts) (ObservabilityMetrics, *http.Response, error) {
 	var (
 		localVarHttpMethod  = strings.ToUpper("Get")
 		localVarPostBody    interface{}
@@ -198,12 +355,13 @@ func (a *ObservabilityApiService) QueryTimeseriesRange(ctx context.Context, proj
 	localVarQueryParams := url.Values{}
 	localVarFormParams := url.Values{}
 
-	if localVarOptionals != nil && localVarOptionals.ResourceType.IsSet() {
-		localVarQueryParams.Add("resource_type", parameterToString(localVarOptionals.ResourceType.Value(), ""))
-	}
+	localVarQueryParams.Add("resource_type", parameterToString(resourceType, ""))
 	if localVarOptionals != nil && localVarOptionals.ResourceId.IsSet() {
 		localVarQueryParams.Add("resource_id", parameterToString(localVarOptionals.ResourceId.Value(), ""))
 	}
+	localVarQueryParams.Add("start", parameterToString(start, ""))
+	localVarQueryParams.Add("end", parameterToString(end, ""))
+	localVarQueryParams.Add("step", parameterToString(step, ""))
 	// to determine the Content-Type header
 	localVarHttpContentTypes := []string{}
 
