397701c8660ab74db115323fc0e0661b1ee34681
Gitlab Runner <pduffy@crusoe.ai>
2026-08-21 18:36:12 +0000

    Client code changes for (CRUSOE-90575: Surface Location Details ListS3Keys) in rest-gw

--- swagger/v1/client.go
+++ swagger/v1/client.go
@@ -54,8 +54,6 @@ type APIClient struct {
 
 	CapacitiesApi *CapacitiesApiService
 
-	CatalogApi *CatalogApiService
-
 	CcrApi *CcrApiService
 
 	ContainerRegistryApi *ContainerRegistryApiService
@@ -194,7 +192,6 @@ func NewAPIClient(cfg *Configuration) *APIClient {
 	c.AutoClustersApi = (*AutoClustersApiService)(&c.common)
 	c.BillingApi = (*BillingApiService)(&c.common)
 	c.CapacitiesApi = (*CapacitiesApiService)(&c.common)
-	c.CatalogApi = (*CatalogApiService)(&c.common)
 	c.CcrApi = (*CcrApiService)(&c.common)
 	c.ContainerRegistryApi = (*ContainerRegistryApiService)(&c.common)
 	c.CreditsApi = (*CreditsApiService)(&c.common)
